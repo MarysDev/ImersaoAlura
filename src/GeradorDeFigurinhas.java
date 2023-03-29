272562d338e8fdeadb40b720ed1374a2b28fcd9d
Marianne <mariannelouren07@gmail>
2023-03-28 23:01:31 -0300

    updated

---
+++ src/GeradorDeFigurinhas.java
@@ -0,0 +1,37 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class GeradorDeFigurinhas {
+  
+    public void cria() throws Exception{
+      // leitura da imagem
+
+      BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpeg"));
+
+      // cria nova imagem em memória com transparência e com tamanho novo
+
+      int largura = imagemOriginal.getWidth();
+      int altura = imagemOriginal.getHeight();
+      int novaAltura = altura + 200;
+      BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+      // copiar a imagem original para a nova imagem (em memória)
+
+      Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+      graphics.drawImage(imagemOriginal,null, 0, 0);
+
+      // escreve uma frase na nova imagem
+
+      // escrever a nova imagem em um arquivo
+      ImageIO.write(novaImagem, null, new File("saida/figurinha.png"));
+    }
+
+    public static void main(String[] args) throws Exception {
+      var geradora = new GeradorDeFigurinhas();
+      geradora.cria();
+    }
+}
